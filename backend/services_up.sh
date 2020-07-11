7ad7a4ee112e3e88e750949f8613ff15e318ed15
NickSolante <41457938+NickSolante@users.noreply.github.com>
2020-07-11 11:31:33 +1000

    setup

---
+++ backend/services_up.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Verify env file is present
+if [ ! -f ./app/.env ]; then
+  echo ".env file not set"
+  exit 1
+fi
+
+# Start the podcast server
+docker-compose up -d redis dbpost 
